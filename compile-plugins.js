11949f28bee690a82ace2bad276d1c1ef5406144
arlen22 <arlenbee@gmail.com>
2019-09-29 17:02:18 -0400

    Update compile-plugins.js

--- compile-plugins.js
+++ compile-plugins.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-
+const crypto = require("crypto");
+const cp = require("child_process");
 //compiles plugins as JSON header plus plain text body
 
 const version = process.argv[3]; 'http-server';
@@ -10,6 +11,7 @@ if (!version || !TWSource) return;
 
 const oldFolder = path.resolve(__dirname, TWSource);
 const newFolder = path.resolve(__dirname, version);
+
 fs.mkdirSync(newFolder);
 var $tw = require(oldFolder).TiddlyWiki();
 $tw.boot.argv = [TWSource + '/editions/empty'];
@@ -19,8 +21,10 @@ $tw.boot.executeNextStartupTask = function () {
 	return true;
 }
 $tw.boot.boot();
+
 function complete() {
-	var test = ['plugins', 'themes'].map(folder => path.join(oldFolder, folder))
+	const hashes = {};
+	['plugins', 'themes'].map(folder => path.join(oldFolder, folder))
 		.reduce((n, folder) => n.concat(fs.readdirSync(folder).filter(sub => !sub.startsWith(".")).map(sub => path.join(folder, sub))), [])
 		.concat([path.join(oldFolder, 'languages')])
 		.reduce((n, folder) => n.concat(fs.readdirSync(folder).filter(sub => !sub.startsWith(".")).map(sub => path.join(folder, sub))), [])
@@ -40,15 +44,16 @@ function complete() {
 				plugin.tiddlers = JSON.parse(plugin.text).tiddlers;
 				delete plugin.text;
 				fs.writeFileSync(path.join(newPath, "plugin.info"), JSON.stringify(plugin));
-			} else console.log(oldpath);
-			// if (plugin) {
-			// 	const text = plugin.text;
-			// 	delete plugin.text;
-			// 	const meta = JSON.stringify(plugin);
-			// 	fs.writeFileSync(path.join(newPath, "plugin.txt"), meta + "\n\n" + text);
-			// } else console.log(oldpath);
-
+				let js = Buffer.from(`$tw.preloadTiddler(${JSON.stringify(plugin)});`, "utf8");
+				let hash = crypto.createHash("sha384").update(js).digest("base64");
+				// console.log(newPath, hash);
+				fs.writeFileSync(path.join(newPath, "plugin.info.js"), js);
+				hashes[relPath] = "sha384-" + hash;
+			} else console.log(oldPath);
 		});
+	console.log(hashes);
+	fs.writeFileSync(path.join(newFolder, "hashes.json"), JSON.stringify(hashes, null, 2));
+
 	fs.mkdirSync(path.join(newFolder, 'boot'));
 	fs.mkdirSync(path.join(newFolder, 'licenses'));
 
@@ -61,6 +66,20 @@ function complete() {
 			console.log("The 'bin' and 'editions' folders are not included because they are not required " +
 				"for TiddlyWiki to work correctly as a module. However, both may be added and the bundle should " +
 				"then work as expected.");
+			let rel = path.relative(path.relative(__dirname, process.argv[2]), newFolder);
+			let cmd = `(cd ${process.argv[2]} && zip -r ${rel}/editions.zip editions)`;
+			console.log("\nRunning %s", cmd);
+			let res = cp.spawnSync(cmd, { shell: true });
+			let output = Buffer.concat(res.output.filter(e => e)).toString("utf8");
+			let outputArr = output.split("\n").map(e => e.trimLeft().startsWith("adding") ? true : e);
+			let count = outputArr.filter(e => e === true).length;
+			let other = outputArr.filter(e => e !== true).join('\n');
+			
+			console.log("Zip finished with status %s and signal %s", res.status, res.signal);
+			console.log(res.stderr.toString("utf8"));
+			console.log("Added %s files, and printed the following lines as well", count);
+			console.log(other);
+
 			return;
 		}
 		const e = files.pop();
